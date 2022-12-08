25baff8f56d13a51bd9d9a5c14556692b6c49c68
Yixin88 <yixin88@hotmail.co.uk>
2022-12-08 21:43:11 +0000

    able to fetch and add card according to category clicked

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const fetchCatagories = () => {
+const fetchListCategories = () => {
     fetch("https://dummyjson.com/products/categories")
     .then(response => response.json())
     .then(data => {
@@ -8,21 +8,45 @@ const fetchCatagories = () => {
             const catagoryBar = document.querySelector(".catagoryNav");
             const linkTag = document.createElement("a");
             linkTag.href = "#";
+            linkTag.setAttribute("onclick", "getCategory(this)")
             linkTag.innerText = catagory;
             catagoryBar.appendChild(linkTag);
         });
     });
 }
 
-fetchCatagories();
+fetchListCategories();
 
-const createCard = (title, image, catagory, rating, price) => {
+const createCard = (title, image, category, rating, price) => {
     const container = document.createElement("div");
     container.classList.add("productCard");
     container.innerHTML = `<img src="${image}">
                             <h2>${title}</h2>
-                            <h3>${catagory}</h3>
+                            <h3>${category}</h3>
                             <span class="rating">${rating}/5 ⭐️</span>
                             <span class="price">£${price}</span>
                             <button>Add to cart</button>`
+    document.querySelector(".selection").appendChild(container);
 }
+
+const clearPage = () => {
+    document.querySelector(".selection").innerHTML = "";
+}
+
+
+const fetchCategory = (category) => {
+    fetch(`https://dummyjson.com/products/category/${category}`)
+    .then(response => response.json())
+    .then(data => {
+        data.products.forEach(item => {
+            createCard(item.title, item.images[0], item.category, item.rating, item.price)
+        })
+    });
+}
+
+const getCategory = (category) => {
+    let categoryName = category.innerText;
+    clearPage();
+    fetchCategory(categoryName);
+}
+
